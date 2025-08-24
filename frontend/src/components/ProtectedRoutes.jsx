d449fa41cf8d138aef2d0720849698104e878242
sambaraks652 <sambaraks652@gmail.com>
2025-08-24 15:54:13 +0300

    Initial commit with backend and frontend

---
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import api from "../api";
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+
+const ProtectedRoutes = () => {
+  return <div></div>;
+};
+
+export default ProtectedRoutes;
