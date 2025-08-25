519667b9bd66d014ceccef93383f565baf18eb0e
sambaraks652 <sambaraks652@gmail.com>
2025-08-25 21:52:31 +0300

    added various pages for navigation in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Home";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoutes";
+import Home from "./pages/Home";
 
-const App = () => {
-  return <div></div>;
-};
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
